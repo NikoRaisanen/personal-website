c0a00a451005f0fe1acba064a70947a6d4e2460d
NikoRaisanen <nikoraisanenbusiness@gmail.com>
2023-01-03 23:27:51 -0700

    created header skeleton and added TODOs

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg';
 
 function App() {
   return (
     <div className="App">
+      {/* TODO: add slowly animated bg to header element */}
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div id="header-text">
+            <h1>Niko Raisanen</h1>
+            <h2>Full-Stack Developer and AppSec Engineer</h2>
+        </div>
+
+        <img src={logo} id="header-pic" alt="headshot"/>
+        
+        <div id="intro">
+            <p>My intro here</p>
+            <p>My intro here</p>
+            <p>My intro here</p>
+        </div>
+        
+        {/*
+        TODO: Add appropriate links to anchor tags
+        TODO: Add svg element into each list item
+        TODO: Remove bullet points from list items
+        */}
+        <div id="socials-bar">
+            <li className="social-icon">
+                <a href='xyz.com'/>
+            </li>
+            <li className="social-icon">
+                <a href='xyz.com'/>
+            </li>
+            <li className="social-icon">
+                <a href='xyz.com'/>
+            </li>
+        </div>
+
       </header>
     </div>
   );
