6c609bfdcb8e64aadbc982126dc6191fda1708cd
unknown <nraisane@asu.edu>
2023-01-06 22:45:51 -0800

    placed first 3 svgs, fixed padding, moved svgs to src dir

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,18 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import logo from '../logo.svg'
+import logo from '../logo.svg';
+import group from '../svg/group.svg';
+import play from '../svg/play.svg';
+import smiley from '../svg/smiley.svg';
 
 const Projects = () => {
     return (
         <div className="projects-container">
             <h2>Projects</h2>
             {/* TODO: refine the hr tag to be more minimalist */}
-            <hr/>
             <div className="projects">
                 {/* TODO: Create first project here, then abstract out to component */}
-                <div className="proj1">
-                    <img src={logo} alt="project"/>
-                </div>
+                <ul className="projects-list">
+                    <li>
+                        <img src={group} alt="project"/>
+                    </li>
+                    <li>
+                        <img src={play} alt="project"/>
+                    </li>
+                    <li>
+                        <img src={smiley} alt="project"/>
+                    </li>
+                </ul>
+                    
             </div>
         </div>
     )
