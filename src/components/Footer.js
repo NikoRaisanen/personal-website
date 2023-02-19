1aff8077e2f8f659bcd8ead0dbf8feb7c6adf456
unknown <nraisane@asu.edu>
2023-02-18 17:34:47 -0800

    added footer text, fixed footer resizing issue

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,8 +14,19 @@ const Footer = () => {
       </a>
       
 
-      <p>blah blah blah a few more things about me here</p>
+      {/* <p>I enjoy combining my cybersecurity knowledge with technical engineering skills to create amazing and secure products. I like to look at the big picture and discuss architectural decisions. Beyond that, I enjoy being an individual contributor and getting into the weeds of technical implementations</p> */}
 
+      {/* <p>
+      I love combining my cybersecurity knoweldge with my technical coding skills to create awesome and secure products. I'm all about looking at the big picture and making informed architectural decisions that align with the company's goals. I'm passionate about securely solving technical problems at both the strategic and individual contributor levels
+      </p> */}
+
+      <p className='footer-text'>
+      I find great joy in leveraging my security and coding skills to craft incredible and secure products. My approach involves taking a holistic view of the project and making informed architectural decisions that are aligned with the company's overall vision and goals. Whether working at a strategic level or as an individual contributor, I'm passionate about delivering elegant and effective solutions that exceed expectations.
+      </p>
+
+      {/* <p>
+      "I derive immense joy from utilizing my expertise in cybersecurity and coding to develop incredible and secure products. My approach involves taking a holistic view of the project and making informed architectural decisions that are aligned with the company's overall vision and goals. Whether working at a strategic level or as an individual contributor, I'm passionate about delivering elegant and effective solutions that exceed expectations."
+      </p> */}
       {/* <ul class="socials">
 
         <li><a href="#"><i class="fa fa-facebook"></i></a></li>
