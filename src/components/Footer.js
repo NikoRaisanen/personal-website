bd7ea61714803d4fbe308703403f880d71ee3849
unknown <nraisane@asu.edu>
2023-02-18 18:01:15 -0800

    change download svg color on hover

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,24 +9,17 @@ const Footer = () => {
     <div class="footer-content">
 
       <h3>Download Resume</h3>
-      <a href='#'>
-        <img src={download} alt="download resume" className='download-icon'/>
+      <a href='#' className='download-anchor'>
+        {/* <img src={download} alt="download resume" className='download-icon'/> */}
+        <svg width="35px" height="35px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M20 15V18C20 19.1046 19.1046 20 18 20H6C4.89543 20 4 19.1046 4 18L4 15M8 11L12 15M12 15L16 11M12 15V3" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" className='download-icon'/>
+</svg>
       </a>
-      
-
-      {/* <p>I enjoy combining my cybersecurity knowledge with technical engineering skills to create amazing and secure products. I like to look at the big picture and discuss architectural decisions. Beyond that, I enjoy being an individual contributor and getting into the weeds of technical implementations</p> */}
-
-      {/* <p>
-      I love combining my cybersecurity knoweldge with my technical coding skills to create awesome and secure products. I'm all about looking at the big picture and making informed architectural decisions that align with the company's goals. I'm passionate about securely solving technical problems at both the strategic and individual contributor levels
-      </p> */}
 
       <p className='footer-text'>
       I find great joy in leveraging my security and coding skills to craft incredible and secure products. My approach involves taking a holistic view of the project and making informed architectural decisions that are aligned with the company's overall vision and goals. Whether working at a strategic level or as an individual contributor, I'm passionate about delivering elegant and effective solutions that exceed expectations.
       </p>
 
-      {/* <p>
-      "I derive immense joy from utilizing my expertise in cybersecurity and coding to develop incredible and secure products. My approach involves taking a holistic view of the project and making informed architectural decisions that are aligned with the company's overall vision and goals. Whether working at a strategic level or as an individual contributor, I'm passionate about delivering elegant and effective solutions that exceed expectations."
-      </p> */}
       {/* <ul class="socials">
 
         <li><a href="#"><i class="fa fa-facebook"></i></a></li>
