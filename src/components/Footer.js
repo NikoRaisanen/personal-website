417b3a8f38e565c0c32ae9f68673bd7f43c4e881
unknown <nraisane@asu.edu>
2023-09-23 17:56:44 -0700

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ const Footer = () => {
           fontSize: '32px',
           paddingRight: '10px',
         }}>Download Resume</p>
-          <svg width="25px" height="25px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <svg style={{paddingTop: '5px'}} width="25px" height="25px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M20 15V18C20 19.1046 19.1046 20 18 20H6C4.89543 20 4 19.1046 4 18L4 15M8 11L12 15M12 15L16 11M12 15V3" stroke="#ffffff" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" className='download-icon'/>
           </svg>
         </a>
@@ -25,7 +25,7 @@ const Footer = () => {
           </p>
         </a>
         <p className='footer-text'>
-        I find great joy in leveraging my security and coding skills to craft secure products that users love. My approach involves taking a holistic view of the project and making informed architectural and code-level decisions that align with the company's overall vision and goals. Whether working at a strategic level or as an individual contributor, I'm passionate about delivering elegant and effective solutions that exceed expectations.
+        I find great joy in leveraging my security software engineering skillset to deliver scalable and secure solutions that withstand the test of time. With a wide range of foundational skills, I'm able to drive projects to completion and quickly adapt to new technologies and environments.  
         </p>
       </div>
     </footer>
