3672e5450aded37a746118ecd6588e85fa198a9d
unknown <nraisane@asu.edu>
2023-11-14 19:15:31 -0800

    remove scrollwheel listener

--- src/App.js
+++ src/App.js
@@ -4,30 +4,11 @@ import Projects from './components/Projects';
 import Socials from './components/Socials';
 import ParticlesBackground from './components/ParticlesBackground';
 import Contributions from './components/Contributions';
-import { useEffect, useRef } from 'react';
+import { useRef } from 'react';
 import Footer from './components/Footer';
 
 function App() {
   const galleryHead = useRef(null);
-
-  useEffect(() => {
-    const el = document.getElementsByClassName('App')[0];
-    el.style.height = '100vh';
-    el.style.overflow = 'hidden';
-    window.addEventListener('wheel', handleScroll, { passive: false });
-  }, []);
-
-  const handleScroll = (e) => {
-    // if scrollwheel up, do nothing
-    if (e.deltaY < 0) return;
-
-    window.removeEventListener('wheel', handleScroll)
-    const el = document.getElementsByClassName('App')[0];
-    el.style.removeProperty('height');
-    el.style.removeProperty('overflow');
-    galleryHead.current.scrollIntoView({ behavior: 'smooth', block: 'start' });
-  }
-
 
   return (
     <div className="App">
