b9766de37487a04c831b13a040eb898b98fadc90
unknown <nraisane@asu.edu>
2023-11-17 19:31:20 -0800

    add copy for my path

--- src/components/ExperiencesList.js
+++ src/components/ExperiencesList.js
@@ -4,20 +4,14 @@ import '../css/experience.css';
 const experiences = [
     {
         id: 1,
-        title: "Penetration Tester",
-        description: "Conducted vulnerability assessments and security audits.",
-        year: "2018-2019",
+        title: "Security Engineering",
+        description: "I got started with Hack The Box and dove into the world of offensive security. Shortly after I earned a top 3% rank in the 2021 National Cyber League CTF competition. I then started work as an Application Security Engineer, building security automation tooling with Python and threat modeling services in the fintech space.",
+        year: "2019-2021",
     },
     {
         id: 2,
-        title: "Security Engineer",
-        description: "Developed and maintained company-wide security protocols.",
-        year: "2020",
-    },
-    {
-        id: 3,
-        title: "Software Engineer",
-        description: "Designed and implemented software solutions with a focus on security.",
+        title: "Software Engineering",
+        description: "Working at a tech startup, I was able to quickly gain knowledge in a wide range of domains from infrastructure, to systems design, to frontend development. With an abundance of autonomy and responsibility, I tackled greenfield projects and optimized existing services to better align with our microservice architecture.",
         year: "2021-Present",
     },
 ];
